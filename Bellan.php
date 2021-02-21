3a475042043818abf79b899677a41e920101bbb9
viktigpetterr <vi1221pe-s@student.lu.se>
2021-02-21 15:59:56 +0100

    Set up cron job configuration

--- Bellan.php
+++ Bellan.php
@@ -4,19 +4,18 @@ use GO\Scheduler;
 use Symfony\Component\Yaml\Yaml;
 use viktigpetterr\bellan\Lunchtime;
 
-$workingHours = Yaml::parse(file_get_contents('working-hours.example.yaml'));
+$workingHours = Yaml::parse(file_get_contents(__DIR__ . '/working-hours.yaml'));
 $days = $workingHours['DAYS'];
 $postAt = $workingHours['POST_AT'];
 [$hour, $minute] = explode(':', $postAt);
 
 (new Scheduler())
     ->call(function () {
-        $config = Yaml::parse(file_get_contents('bellan.example.yaml'));
-        $webhook = $config['WEB_HOOK'];
-        $restaurants = $config['RESTAURANTS'];
-        (new Lunchtime($webhook, $restaurants))->execute();
+        $config = Yaml::parse(file_get_contents(__DIR__ . '/bellan.yaml'));
+        return (new Lunchtime($config['WEB_HOOK'], $config['RESTAURANTS']))->execute();
     })
     ->at("$minute $hour * * $days")
+    ->output(__DIR__ . '/bellan.log')
     ->run();
 
 
